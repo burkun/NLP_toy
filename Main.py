df7dda54f2188b6408c56c34aa46bd643df647a9
andrew peiken <tingoo@live.cn>
2013-11-30 09:38:11 +0800

    delete

--- Main.py
+++
@@ -1,7 +0,0 @@
-'''
-Created on 2013年11月19日
-https://github.com/burkun/NLP_pro1.git
-@author: Administrator
-'''
-
-print("hello nlp!")
--- README.md
+++
@@ -1,6 +0,0 @@
-NLP_pro1
-========
-
-自然语言处理project1
-
-https://github.com/burkun/NLP_pro1.git
