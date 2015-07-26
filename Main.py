efc24eb8996eb64f81528bd7332ec2ce68e938db
andrew peiken <616651082@qq.com>
2015-07-26 17:58:54 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,14 +8,21 @@ test module
 #import util.Preprocessing as pre
 #pre.writeWeiboData()
 #pre.writeGIGAData()
-pp = "E:\ChinaGIGA\data_0.txt"
-def cuttest(test_sent):
-    result = jieba.cut(test_sent)
-    print " / ".join(result)
-import jieba
-from util.File import FileIO
+# import util.GenUserDic as gen,util.Config as conf
+# dataPath = conf.OUTPUTDIR_CHINAGIGA
+# dic = gen.UserDic()
+# dic.genDic(dataPath)
 
-res = FileIO.readProFile(pp)
+#减少词典中无关词的个数，没用过
+# import util.reduceDic as reduc
+# reduc.reduceDic()
 
+#生成新词
+# import util.genNewWord as newWord 
+# import pickle
+# newWord.gen()
 
-cuttest(res[0])
+import pickle
+data = pickle.load(open("newWordDic.pickle"))
+for key in data:
+    print key, data[key][0]
